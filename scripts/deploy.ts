dd24d195119adbe5b63481030bff41000c795627
Maxime Lalo <maxime.lalo.pro@gmail.com>
2022-12-15 12:18:32 +0100

    :sparkles: Deploy working

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,7 +1,8 @@
 import { buf2hex } from "@taquito/utils";
 import { InMemorySigner } from "@taquito/signer";
 import { MichelsonMap, TezosToolkit } from "@taquito/taquito";
-import code from "../src/compiled/main.json";
+import code_contract_1 from "../src/compiled/contract_1.json";
+import code_contract_2 from "../src/compiled/contract_2.json";
 import dotenv from "dotenv";
 import metadata from "./metadata/main.json";
 import path from "path";
@@ -12,26 +13,53 @@ dotenv.config({ path: path.join(__dirname, "..", ".env") });
 // Initialize RPC connection
 const Tezos = new TezosToolkit(process.env.NODE_URL || "");
 
-const deploy = async () => {
+(async () => {
     try {
         const signer = await InMemorySigner.fromSecretKey(
             process.env.ADMIN_SK || ""
         );
         const admin: string = await signer.publicKeyHash();
         Tezos.setProvider({ signer });
+    } catch (e) {
+        console.log(e);
+    }
 
-        const storage = 0;
-
-        const op = await Tezos.contract.originate({ code, storage });
+    async function deploy_contract_1() {
+        const storage = {
+            user_map: new MichelsonMap(),
+            user_blacklist: [],
+            admin_list: new MichelsonMap(),
+            has_paid: new MichelsonMap(),
+        };
+        const op = await Tezos.contract.originate({
+            code: code_contract_1,
+            storage: storage,
+        });
         await op.confirmation();
-        console.log(`[OK] Token FA2: ${op.contractAddress}`);
+        console.log(`[OK] Contract 1: ${op.contractAddress}`);
         // check contract storage with CLI
         console.log(
             `tezos-client --endpoint http://localhost:20000 get contract storage for ${op.contractAddress}`
         );
-    } catch (e) {
-        console.log(e);
     }
-};
 
-deploy();
+    async function deploy_contract_2() {
+        const storage = {
+            admin: "tz1cGkwCNGQqeA5BcAUqi8KoZxwmLfMkJEbR",
+            tezos_price: new MichelsonMap(),
+        };
+        const op = await Tezos.contract.originate({
+            code: code_contract_2,
+            storage: storage,
+        });
+        await op.confirmation();
+        console.log(`[OK] Contract 2: ${op.contractAddress}`);
+        // check contract storage with CLI
+        console.log(
+            `tezos-client --endpoint http://localhost:20000 get contract storage for ${op.contractAddress}`
+        );
+    }
+
+    await deploy_contract_1();
+    await deploy_contract_2();
+})();
